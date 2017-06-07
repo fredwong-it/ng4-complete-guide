002c26e189b57626c84e73b1ccc82a434cac3ce4
Fred Wong <fredwong@Freds-MacBook-Pro.local>
2017-06-06 23:29:40 -0400

    add basic component, recipe, shopping-list, header

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test recipe 1", "This is a test recipe 1", "http://s345780157.onlinehome.us/wp-content/uploads/2011/03/Rosh-Hashanah-chicken-with-apples.jpg")
+  ];
 
   constructor() { }
 
