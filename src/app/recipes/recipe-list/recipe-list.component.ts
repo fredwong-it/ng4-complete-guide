7630ac1c25d100fbfd7d34ce4757dde621935b23
fred <fredwong.it@gmail.com>
2017-06-11 16:16:46 -0400

    select recipe to display recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -8,12 +8,18 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("Test recipe 1", "This is a test recipe 1", "http://s345780157.onlinehome.us/wp-content/uploads/2011/03/Rosh-Hashanah-chicken-with-apples.jpg")
+    new Recipe("Test recipe 1", "This is a test recipe 1", "http://s345780157.onlinehome.us/wp-content/uploads/2011/03/Rosh-Hashanah-chicken-with-apples.jpg"),
+    new Recipe("Apple recipe", "This is a apple recipe", "http://s345780157.onlinehome.us/wp-content/uploads/2011/03/Rosh-Hashanah-chicken-with-apples.jpg")
   ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelect(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
