4897016bd1a6bf8086ee40b7a2adc55395807f1c
Fred Wong <fredwong.it@gmail.com>
2017-06-22 14:20:15 -0400

    used template-driven form in shopping-edit and  add validation and clear

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model'
 import { ShoppingListService } from 'app/shopping-list/shopping-list.service';
@@ -8,18 +9,23 @@ import { ShoppingListService } from 'app/shopping-list/shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('f') editForm: NgForm;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddIngredient(nameInput: HTMLInputElement) {
-    const name = nameInput.value;
-    const amount = this.amountInput.nativeElement.value;
-    const ingredient = new Ingredient(name, amount);
+  onAddItem() {
+    console.log(this.editForm);
+
+    const value = this.editForm.value;
+    const ingredient = new Ingredient(value.name, value.amount);
 
     this.shoppingListService.addIngredient(ingredient);
+  }
+
+  onClear() {
+    this.editForm.reset();
   }
 }
